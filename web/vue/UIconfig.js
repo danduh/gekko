731b119bd505799b5624701d56cb3491971f80f2
Daniel Ostrovsky <danielo@stuccomedia.com>
2017-12-23 23:21:41 +0200

    config

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -4,22 +4,38 @@
 // see https://github.com/askmike/gekko/blob/stable/docs/installing_gekko_on_a_server.md
 
 const CONFIG = {
-  headless: false,
+  headless: true,
   api: {
     host: '127.0.0.1',
-    port: 3000,
-    timeout: 120000 // 2 minutes
+    port: 3000
   },
   ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
-    path: '/'
+    ssl: true,
+    host: 'test.monitorcrypto.io',
+    port: 443,
+    path: '/' // change this if you are serving from something like `example.com/gekko`
   },
   adapter: 'sqlite'
-}
+};
 
-if(typeof window === 'undefined')
-  module.exports = CONFIG;
-else
-  window.CONFIG = CONFIG;
+//
+// const CONFIG = {
+//   headless: false,
+//   api: {
+//     host: '127.0.0.1',
+//     port: 3000,
+//     timeout: 120000 // 2 minutes
+//   },
+//   ui: {
+//     ssl: false,
+//     host: 'localhost',
+//     port: 3000,
+//     path: '/'
+//   },
+//   adapter: 'sqlite'
+// }
+//
+// if(typeof window === 'undefined')
+//   module.exports = CONFIG;
+// else
+//   window.CONFIG = CONFIG;
